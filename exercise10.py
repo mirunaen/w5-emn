fa08ede09cb6544478245e0a7beafe8e4240bee9
Miruna <miruna.neds@gmail.com>
2020-10-18 22:27:53 +0000

    comments

--- exercise10.py
+++ exercise10.py
@@ -1,3 +1,4 @@
+# Create a program that asks the user to enter a number and keeps asking until a number is introduced. 
 n=input("Enter a num:")
 while n.isdigit() == False and "." not in n:
   n=input("Write a num:")
